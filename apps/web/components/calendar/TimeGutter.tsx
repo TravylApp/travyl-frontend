5c910364b21c92d72dd2fb84f22478b3c43c99db
justi <jlo2@csub.edu>
2026-03-16 16:01:20 -0700

    feat: add TimeGutter component

---
+++ apps/web/components/calendar/TimeGutter.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { HOUR_HEIGHT } from './constants'
+import { formatHourGutter } from './utils'
+import type { TimeRange } from './types'
+
+interface TimeGutterProps { timeRange: TimeRange }
+
+export function TimeGutter({ timeRange }: TimeGutterProps) {
+  const hours: number[] = []
+  for (let h = timeRange.startHour; h <= timeRange.endHour; h++) hours.push(h)
+  return (
+    <div className="relative flex-shrink-0 w-14 text-right pr-3">
+      {hours.map((hour) => (
+        <div key={hour} className="relative text-xs text-gray-400 select-none" style={{ height: HOUR_HEIGHT }}>
+          <span className="absolute -top-2 right-3">{formatHourGutter(hour)}</span>
+        </div>
+      ))}
+    </div>
+  )
+}
