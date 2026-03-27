7c1a7d410c3ed00ca6d8ef2a671aeef72b876a72
justi <jlo2@csub.edu>
2026-03-26 17:43:22 -0700

    fix(infra): pass SUPABASE_SECRET_KEY to Next.js site environment

--- infra/web.ts
+++ infra/web.ts
@@ -28,6 +28,7 @@ export const site = new sst.aws.Nextjs('TravylWeb', {
     NEXT_PUBLIC_SUPABASE_URL: supabaseUrl.value,
     NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: supabasePublishableKey.value,
     NEXT_PUBLIC_RECOMMENDATION_API_URL: api.url,
+    SUPABASE_SECRET_KEY: supabaseSecretKey.value,
     PEXELS_API_KEY: pexels.value,
   },
 })
